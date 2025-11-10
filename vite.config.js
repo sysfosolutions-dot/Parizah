5629e9ed8836dbc2ecd0d934cc9ddf1c0c0df152
HarshPrajapati4926 <harshprajapati4926@gmail.com>
2025-11-10 13:42:06 +0530

    changes in header for sub menu on hover

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/multikart/userarea/',
+  base:'/',
   css: {
     devSourcemap: true, // 👈 enabling css
   },
